77191a55a12708fff9c0de03b6ba869f9099fb76
elyasdehati <elyas786dehaty@gmail.com>
2025-07-30 15:15:47 +0430

    Add Eloquent relationships to Purchase model

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Purchase extends Model
 {
     protected $guarded = [];
+
+    public function purchaseItem(){
+        return $this->hasMany(PurchaseItem::class, 'purchase_id');
+    }
+
+    public function supplier(){
+        return $this->belongsTo(Supplier::class, 'supplier_id');
+    }
+
+    public function warehouse(){
+        return $this->belongsTo(warehouse::class, 'warehouse_id');
+    }
+
+    public function product(){
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
 }
